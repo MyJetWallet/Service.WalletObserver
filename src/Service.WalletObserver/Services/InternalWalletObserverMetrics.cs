742a59ea52103043587a93b0f3391b08952cfcb0
geomatika46 <alexey.p@smplt.net>
2021-08-20 11:45:35 +0300

    fix bug with mertics

--- src/Service.WalletObserver/Services/InternalWalletObserverMetrics.cs
+++ src/Service.WalletObserver/Services/InternalWalletObserverMetrics.cs
@@ -1,3 +1,6 @@
+using System;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Prometheus;
 using Service.WalletObserver.Domain.Models;
 
@@ -5,47 +8,61 @@ namespace Service.WalletObserver.Services
 {
     public class InternalWalletObserverMetrics
     {
+        private readonly ILogger<InternalWalletObserverMetrics> _logger;
+        
         private static readonly Gauge InternalWalletBalanceUsd = Metrics
-            .CreateGauge("jetwallet-internalwallet-balance-usd",
+            .CreateGauge("jetwallet_internalwallet_balance_usd",
                 "Usd balance by asset and wallet.",
                 new GaugeConfiguration { LabelNames = new[] { "asset", "wallet"} });
         
         private static readonly Gauge InternalWalletBalance = Metrics
-            .CreateGauge("jetwallet-internalwallet-balance",
+            .CreateGauge("jetwallet_internalwallet_balance",
                 "Balance by asset and wallet.",
                 new GaugeConfiguration { LabelNames = new[] { "asset", "wallet"} });
         
         private static readonly Gauge InternalWalletBalanceLimit = Metrics
-            .CreateGauge("jetwallet-internalwallet-balance-limit",
+            .CreateGauge("jetwallet_internalwallet_balance_limit",
                 "Balance limit by asset and wallet.",
                 new GaugeConfiguration { LabelNames = new[] { "asset", "wallet"} });
         
         private static readonly Gauge InternalWalletBalanceAlert = Metrics
-            .CreateGauge("jetwallet-internalwallet-balance-alert",
+            .CreateGauge("jetwallet_internalwallet_balance_alert",
                 "Balance alert by asset and wallet.",
                 new GaugeConfiguration { LabelNames = new[] { "asset", "wallet"} });
 
+        public InternalWalletObserverMetrics(ILogger<InternalWalletObserverMetrics> logger)
+        {
+            _logger = logger;
+        }
+
         public void SetMetrics(InternalWalletBalance balance)
         {
-            InternalWalletBalanceUsd
-                .WithLabels(balance.Asset, balance.WalletName)
-                .Set((double) balance.UsdVolume);
-            
-            InternalWalletBalance
-                .WithLabels(balance.Asset, balance.WalletName)
-                .Set((double) balance.Volume);
-            
-            InternalWalletBalanceLimit
-                .WithLabels(balance.Asset, balance.WalletName)
-                .Set((double) balance.MinBalanceInUsd);
+            try
+            {
+                InternalWalletBalanceUsd
+                    .WithLabels(balance.Asset, balance.WalletName)
+                    .Set(Convert.ToDouble(balance.UsdVolume));
 
-            var valueForAlert = balance.UsdVolume < balance.MinBalanceInUsd
-                ? balance.UsdVolume
-                : 0;
-            
-            InternalWalletBalanceAlert
-                .WithLabels(balance.Asset, balance.WalletName)
-                .Set((double) valueForAlert);
+                InternalWalletBalance
+                    .WithLabels(balance.Asset, balance.WalletName)
+                    .Set((double) balance.Volume);
+
+                InternalWalletBalanceLimit
+                    .WithLabels(balance.Asset, balance.WalletName)
+                    .Set((double) balance.MinBalanceInUsd);
+
+                var valueForAlert = balance.UsdVolume < balance.MinBalanceInUsd
+                    ? balance.UsdVolume
+                    : 0;
+
+                InternalWalletBalanceAlert
+                    .WithLabels(balance.Asset, balance.WalletName)
+                    .Set((double) valueForAlert);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogDebug($"InternalWalletObserverMetrics throw ex: {JsonConvert.SerializeObject(ex)}");
+            }
         }
     }
 }
