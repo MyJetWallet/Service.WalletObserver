98f1e77072b445f2dbd1beac0f5cfa3bf3fa41ef
Maxim Gorin <maksim.g@smplt.net>
2022-05-13 13:29:50 +0300

    Fix build after service sdk update

--- src/Service.WalletObserver/Program.cs
+++ src/Service.WalletObserver/Program.cs
@@ -36,7 +36,7 @@ namespace Service.WalletObserver
 
             Settings = SettingsReader.GetSettings<SettingsModel>(SettingsFileName);
 
-            using var loggerFactory = LogConfigurator.ConfigureElk("MyJetWallet", Settings.SeqServiceUrl, Settings.ElkLogs);
+            using var loggerFactory = LogConfigurator.ConfigureElk_v2("MyJetWallet", Settings.SeqServiceUrl, Settings.ElkLogs);
 
             var logger = loggerFactory.CreateLogger<Program>();
 
